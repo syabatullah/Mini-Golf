bf1ebfdcfe20ae4c6c62f7564c67b410cefc991f
syabatullah <ahmadjumadiaden17@gmail.com>
2023-05-14 16:50:01 +0700

    Mini Golf

---
+++ Assets/script/LevelData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Class which store level data
+/// </summary>
+[System.Serializable]
+public class LevelData
+{
+    public int shotCount;           //maximum shot player can take 
+	public string scene;
+	public void Namescene(){
+		SceneManager.LoadScene (scene);
+	}
+}
---
+++ Assets/script/golf.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class golf : MonoBehaviour
+{
+	[SerializeField] private float shotPower;
+	[SerializeField] private float stopVelocity = .05f;
+	[SerializeField] private LineRenderer lineRenderer;
+
+	private bool isIdle;
+	private bool isAiming;
+
+	private Rigidbody rigidbody;
+
+	private void Awake(){
+		rigidbody = GetComponent<Rigidbody>();
+
+		isAiming = false;
+		lineRenderer.enabled = false;
+
+	}
+
+	private void Update(){
+		if(rigidbody.velocity.magnitude < stopVelocity){
+			
+		}
+		ProcessAim();
+	}
+
+	private void OnMouseDown(){
+		if(isIdle){
+			isAiming = true;
+		}
+	}
+
+	private void ProcessAim(){
+		if(!isAiming || !isIdle){
+			return;
+		}
+			Vector3? worldPoint = CastMouseClickRay();
+			if (!worldPoint.HasValue){
+			return;
+		}
+		DrawLine(worldPoint.Value);
+
+		if(Input.GetMouseButtonUp(0)){
+			Shoot(worldPoint.Value);
+		}
+	}
+
+	private void Shoot(Vector3 worldPoint){
+		isAiming = false;
+		lineRenderer.enabled = false;
+
+		Vector3 horizontalWorldPoint = new Vector3(worldPoint.x , transform.position.y , worldPoint.z);
+
+		Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
+		float strenght = Vector3.Distance(transform.position,horizontalWorldPoint);
+
+		rigidbody.AddForce(direction * strenght * shotPower);
+	}
+
+	private void DrawLine(Vector3  worldPoint){
+		Vector3 [] positions = {
+			transform.position,  worldPoint};
+		lineRenderer.SetPositions(positions);
+		lineRenderer.enabled = true;
+		
+	}
+
+	private void Stop(){
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
+		isIdle = true;
+	}
+
+
+
+	private Vector3? CastMouseClickRay(){
+		Vector3 screenMousePosFar = new Vector3(
+		Input.mousePosition.x,
+		Input.mousePosition.y,
+			Camera.main.farClipPlane);
+
+		Vector3 screenMousePosNear = new Vector3(
+			Input.mousePosition.x,
+			Input.mousePosition.y,
+			Camera.main.nearClipPlane);
+		Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
+		Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
+		RaycastHit hit;
+		if(Physics.Raycast(worldMousePosNear, worldMousePosFar - worldMousePosNear, out hit, float.PositiveInfinity)){
+			return hit.point;
+		}else{
+			return null;
+		}
+	}
+}
